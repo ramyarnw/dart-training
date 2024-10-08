2ec5574170411b28a60bc0a2420ff714518a4372
ramyarnw <ramya.r@nativewit.in>
2024-10-08 15:46:18 +0530

    list functions added

---
+++ lib/asmap.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  var words = <String>['fee', 'fi', 'fo', 'fum'];
+  var map = words.asMap();  // {0: fee, 1: fi, 2: fo, 3: fum}
+  var map1 = map.keys.toList(); // [0, 1, 2, 3]
+  print(map1);
+  final terrestrial = <int, String>{1: 'Mercury', 0: 'Venus', 10: 'Earth'};
+  terrestrial.removeWhere((key, value) => value==0);
+  print(terrestrial);
+
+}
