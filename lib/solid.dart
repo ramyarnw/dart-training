c1722cfaf98cf15bc217945c3fc41357285c7221
ramyarnw <ramya.r@nativewit.in>
2024-10-10 16:20:11 +0530

    solid added

---
+++ lib/solid.dart
@@ -0,0 +1,22 @@
+class Vehicle {
+  int getNumberOfWheels() {
+    return 4;
+  }
+}
+
+class Bicycle extends Vehicle {
+  int getNumberOfWheels() {
+    return 2;
+  }
+}
+
+// calling code
+int COST_PER_TIRE = 50;
+int tireCost(Vehicle vehicle) {
+  return COST_PER_TIRE * vehicle.getNumberOfWheels();
+}
+void main()
+{
+Bicycle bicycle = new Bicycle();
+print(tireCost(bicycle));
+}
